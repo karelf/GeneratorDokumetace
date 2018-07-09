24f24ab8db1dde89aacf16d37f7002fe63789fa2
Karel Foršt <karel.forst@anritsu.com>
2018-07-09 20:15:47 +0200

    First commit

---
+++ bin/rn-test.bat
@@ -0,0 +1,3 @@
+doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc C:\Users\cz040124\Documents\Anritsu\XMLgenerator\bin\createDoc\RN-test-1.doc
+doc-generator.bat Vzor_changes.xml x.xml _str-test.doc C:\Users\cz040124\Documents\Anritsu\XMLgenerator\bin\createDoc\STR-test-1.doc
+doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc C:\Users\cz040124\Documents\Anritsu\XMLgenerator\bin\createDoc\MAIL-test-1.doc
